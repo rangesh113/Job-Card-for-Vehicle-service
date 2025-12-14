5589ee0b00b8f14051eaf674d78fda2a31ce562c
rangesh113 <kingboys113@gmail.com>
2025-12-14 19:02:18 +0530

    Fixed background image by moving to src/assets and applying via React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from "react";
 import Login from "./pages/Login";
 import Navbar from "./components/Navbar";
 import PrivateRoute from "./auth/PrivateRoute";
+import garageBackground from "./assets/garage-background.jpg";
 
 // DASHBOARDS
 import ManagerDashboard from "./pages/manager/Dashboard";
@@ -13,6 +15,21 @@ import CashierDashboard from "./pages/cashier/CashierDashboard";
 
 const Layout = ({ children }) => {
   const location = useLocation();
+
+  // Apply background image to body
+  useEffect(() => {
+    document.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${garageBackground})`;
+    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundPosition = 'center';
+    document.body.style.backgroundAttachment = 'fixed';
+    document.body.style.backgroundRepeat = 'no-repeat';
+
+    return () => {
+      // Cleanup on unmount
+      document.body.style.backgroundImage = '';
+    };
+  }, []);
+
   return (
     <>
       {location.pathname !== "/" && <Navbar />}
